fa7ff782e41a8d4dcacf6ae4c881370f8c3c5f9e
yulafezmesi <certel@hotmail.com>
2021-04-29 00:23:22 +0300

    .env file added pathname

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 const express = require('express')
 const app = express()
 const indexRouter = require('./routes/index')
